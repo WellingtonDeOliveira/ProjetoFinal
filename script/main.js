6515b7db65d775adddbf470177b8fbb19e35ce96
Wellington de Oliveira <wellington220100@gmail.com>
2021-09-11 13:14:45 -0300

    Tela detalhes

--- script/main.js
+++ script/main.js
@@ -2,21 +2,27 @@ jQuery(document).ready(function ($) {
     
     var x = document.documentElement.scrollHeight;
     window.onscroll = function () {
-        if (window.pageYOffset > 140) {
+        if (window.pageYOffset > 130) {
+            if(window.location.pathname == "/C:/xampp/htdocs/projetofinal/esboco/detalhePedido.html"){
+                $("#cima").addClass("activeDif");
+                $("#detalhes").addClass("active");
+            }
             $("#cima").addClass("active");
             $("#filtro").addClass("active");
             $("#cardapio").addClass("active");
             var img = document.querySelector("#logo");
             img.setAttribute('src', './assets/logoS.png');
-            //alert(x);
+            //alert(y);
         } else {
             $("#cima").removeClass("active");
+            $("#cima").removeClass("activeDif");
+            $("#detalhes").removeClass("active");
             $("#filtro").removeClass("active");
             $("#cardapio").removeClass("active");
             var img = document.querySelector("#logo");
             img.setAttribute('src', './assets/logoG.png');
         }
-        if (window.pageYOffset > x-830) {
+        if (window.pageYOffset > x-940) {
             $("#carrinho").addClass("active");
             $("#baixo").addClass("active");
         } else {
